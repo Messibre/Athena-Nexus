5fe6838d1656f0efcf04ae3a86906c7493af941d
Messibre <messibre27@gmail.com>
2026-03-24 00:56:32 +0300

    fixed motion bug

--- client/src/pages/Milestones.js
+++ client/src/pages/Milestones.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useMemo, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
+import { motion } from "framer-motion";
 import { CheckCircle2, ChevronRight, ChevronLeft, Lock } from "lucide-react";
 import Navbar from "../components/Navbar";
 import {
